8789f45e41dfa779baa2971f1871e7417701286f
Stupeter-X <1103254414@qq.com>
2025-10-22 14:19:02 +0800

    asr-test-xyl-2025/10/22/14:18

--- models/model_management.py
+++ models/model_management.py
@@ -7,17 +7,14 @@ load_dotenv()
 
 
 # 创建一个模型管理类
-class MyModel:
+class LLMModel:
     # 创建一个模型变量
     _llm = None
-    # 创建一个词嵌入变量
-    _asr = None
+
     # 构造函数
     def __init__(self):
         # 获取模型名称
         self._llm_name = os.getenv("LLM_NAME")
-        self._asr_name = os.getenv("ASR_NAME")
-
 
     # 加载千问模型
     def get_line_model(self):
@@ -26,16 +23,8 @@ class MyModel:
             self._llm = ChatOpenAI(model=self._llm_name)
         return self._llm
 
-    # 加载ASR模型
-    def get_asr_model(self):
-        if self._asr is None:
-            self._asr = ChatOpenAI(model=self._asr_name)
-        return self._asr
-
 if __name__ == '__main__':
-    model = MyModel()
+    model = LLMModel()
     llm = model.get_line_model()
     res = llm.invoke("hello world, 用一句中文诗词回答，只返回答案，不要解释。")
     print(res.content)
-    asr = model.get_asr_model()
-    res = asr.invoke()
